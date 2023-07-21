5f75abce9b742973f942a6dfdffe1dc09deaa67e
LupusEternus <wilk.tomek@gmail.com>
2023-07-21 15:04:28 +0200

    added methods to MenuServiceImpl

--- src/main/java/service/impl/MenuServiceImpl.java
+++ src/main/java/service/impl/MenuServiceImpl.java
@@ -1,6 +1,54 @@
 package service.impl;
 
-//TODO IMPLEMENT
-public class MenuServiceImpl {
+import domain.DietType;
+import domain.eto.Meal;
+import domain.eto.Produce;
+import service.api.MenuService;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+//TODO IMPLEMENT
+public class MenuServiceImpl implements MenuService {
+
+    @Override
+    public List<Meal> findVegetarianFood(List<Meal> meals) {
+        return meals.stream().filter(Objects::nonNull).filter(meal -> meal.getDietType().equals(DietType.VEGETARIAN))
+                .collect(Collectors.toList());
+
+    }
+
+    @Override
+    public List<Meal> findFoodByType(List<Meal> meals, DietType diet) {
+        return meals.stream().filter(meal -> meal.getDietType().equals(diet)).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Meal> findFoodByName(List<Meal> meals, Integer price) {
+        return meals.stream().filter(meal -> meal.getPrice() < price).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Meal> findFoodCheaperWithCalories(List<Meal> meals, Integer minCalories, Integer maxCalories) {
+        return meals.stream().filter(meal -> meal.getCalories() > minCalories && meal.getCalories() < maxCalories)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Meal> findFoodByName(List<Meal> meals, String name) {
+        return meals.stream().filter(meal -> meal.getName().toLowerCase().startsWith(name.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Meal> findFoodContaining(List<Meal> meals, Produce product) {
+        return meals.stream().filter(meal -> meal.getProducts().contains(product)).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Meal> findFoodExcludingAll(List<Meal> meals, List<Produce> products) {
+        return meals.stream().filter(meal -> Collections.disjoint(meal.getProducts(),products)).collect(Collectors.toList());
+    }
 }
